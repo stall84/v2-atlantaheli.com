32b298937f8a7a19e87931d44b1201bbedcbeec7
Michael Stallings <stall84@gmail.com>
2021-07-17 16:17:59 -0400

    Initial Commit

---
+++ src/pages/tours.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Layout from 'components/Layout';
+import SEO from 'components/SEO';
+
+
+const ToursPage: React.FC = () => {
+    return (
+        <Layout>
+            <SEO title="Charters and Tours" />
+            <p>This is the Tours Page</p>
+        </Layout>
+    );
+};
+
+export default ToursPage;
