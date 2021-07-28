c9b45e2d6f73e4fc11e4154237c415bff90c072f
Michael Stallings <stall84@gmail.com>
2021-07-28 07:05:46 -0400

    Succeeded in wiring up the banner-photo to poll with graphql static query at build time. still need to integrate with sanity

--- src/components/HeroBanner/index.tsx
+++ src/components/HeroBanner/index.tsx
@@ -4,6 +4,7 @@ import { useStaticQuery, graphql } from 'gatsby';
 import Banner from 'components/ui/Banner';
 
 import { SectionTitle } from 'helpers/definitions';
+import { GatsbyImage } from 'gatsby-plugin-image';
 
 interface SectionHeroBanner extends SectionTitle {
   content: string;
@@ -11,8 +12,10 @@ interface SectionHeroBanner extends SectionTitle {
   linkText: string;
 }
 
+
+
 const HeroBanner: React.FC = () => {
-  const { markdownRemark } = useStaticQuery(graphql`
+  const { markdownRemark, bgImage } = useStaticQuery(graphql`
     query {
       markdownRemark(frontmatter: { category: { eq: "hero section" } }) {
         frontmatter {
@@ -23,19 +26,29 @@ const HeroBanner: React.FC = () => {
           linkText
         }
       }
+      bgImage: file(relativePath: { eq: "helosbg.webp" }) {
+        childImageSharp {
+          gatsbyImageData(layout: FULL_WIDTH)
+        }
+      }
     }
   `);
 
   const heroBanner: SectionHeroBanner = markdownRemark.frontmatter;
-
+  const backgroundImage = bgImage.childImageSharp.gatsbyImageData;
+  console.log('BG: ', backgroundImage)
   return (
-    <Banner
-      title={heroBanner.title}
-      subtitle={heroBanner.subtitle}
-      content={heroBanner.content}
-      linkTo={heroBanner.linkTo}
-      linkText={heroBanner.linkText}
-    />
+    <>
+      <GatsbyImage image={backgroundImage} alt="Backgroun Image" />
+      <Banner
+        title={heroBanner.title}
+        subtitle={heroBanner.subtitle}
+        content={heroBanner.content}
+        linkTo={heroBanner.linkTo}
+        linkText={heroBanner.linkText}
+        bgImg={backgroundImage}
+      />
+    </>
   );
 };
 
--- src/components/ui/Banner/index.tsx
+++ src/components/ui/Banner/index.tsx
@@ -4,7 +4,7 @@ import { Link } from 'gatsby';
 import Container from 'components/ui/Container';
 import Button from 'components/ui/Button';
 import TitleSection from 'components/ui/TitleSection';
-
+import { StaticImage } from 'gatsby-plugin-image';
 import * as Styled from './styles';
 
 interface Props {
@@ -13,18 +13,22 @@ interface Props {
   content: React.ReactNode;
   linkTo: string;
   linkText: string;
+  bgImg: string;
 }
 
-const Banner: React.FC<Props> = ({ title, subtitle, content, linkTo, linkText }) => (
-  <Styled.Banner>
-    <Container section>
-      <TitleSection title={title} subtitle={subtitle} hero none center />
-      <Styled.Content>{content}</Styled.Content>
-      <Link to={linkTo}>
-        <Button primary>{linkText}</Button>
-      </Link>
-    </Container>
-  </Styled.Banner>
+const Banner: React.FC<Props> = ({ title, subtitle, content, linkTo, linkText, bgImg }) => (
+  <>
+    {/* <StaticImage src={bgImg} alt="background image of Atlanta with helicopters" /> */}
+    <Styled.Banner>
+      <Container section>
+        <TitleSection title={title} subtitle={subtitle} hero none center />
+        <Styled.Content>{content}</Styled.Content>
+        <Link to={linkTo}>
+          <Button primary>{linkText}</Button>
+        </Link>
+      </Container>
+    </Styled.Banner>
+  </>
 );
 
 export default Banner;
