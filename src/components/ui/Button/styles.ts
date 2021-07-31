a418fb9c83e4e0b7cd4fdc0f7bd3b13c7962b1fb
Michael Stallings <stall84@gmail.com>
2021-07-30 21:50:04 -0400

    Added framer motion to all infoblocks in services and contacts.. changed footer to placeholder social media.. sorted the xola api tour array to render them ascending in price. Will hold major changes and additions and get ready to detail for first production run after client test/feedback

--- src/components/ui/Button/styles.ts
+++ src/components/ui/Button/styles.ts
@@ -5,16 +5,14 @@ import { motion } from 'framer-motion';
 export interface StyledProps {
   primary?: boolean;
   block?: boolean;
-  hover?: boolean;
 }
 
 export const Button = motion.custom(styled.button<StyledProps>`
   outline: none !important;
-  ${tw`py-2 px-8 rounded-full border border-black text-indigo-900`};
+  ${tw`py-2 px-8 rounded-full border border-black text-black hover:bg-offWhite`};
 
   ${({ primary }) => (primary ? tw`bg-lightRed` : tw`text-black`)};
 
   ${({ block }) => block && tw`w-full`};
 
-  ${({ hover }) => hover && tw`hover:bg-teal-500`};
 `);
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
       colors: {
         red: '#d42a2a',
         lightRed: '#c85a57',
-        black: '#1f1f1f'
+        black: '#1f1f1f',
+        offWhite: '#f3f1f0',
+        slate: '#636464',
+        teal2: '#2ad4d4',
+        blue2: '#2a7fd4',
+        green2: '#2ad47f'
+      },
+      height: {
+        mobile: '4rem'
+      },
+      width: {
+        mobile: '8rem'
       }
     }
   },
