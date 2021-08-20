a89f62802c770730e342c254b37bcdf46462d056
Michael Stallings <stall84@gmail.com>
2021-08-20 13:33:13 -0400

    Commented out numerous sections such as blog, blog template, experiences, newsletter and education, and in some cases like experiences deleted the component outright with it's data. We're not coding these parts of the site out for a while, and when we do they'll be pulling from Sanity and not from files present in repo at build time. Commented out gql queries for logo in Header/Logo to bypass build errors

--- src/components/Skills/styles.ts
+++ src/components/Skills/styles.ts
@@ -1,10 +1,12 @@
-import styled from 'styled-components';
-import tw from 'tailwind.macro';
+// import styled from 'styled-components';
+// import tw from 'tailwind.macro';
 
-export const Skills = styled.div`
-  ${tw`flex flex-wrap w-full`};
-`;
+// export const Skills = styled.div`
+//   ${tw`flex flex-wrap w-full`};
+// `;
 
-export const Skill = styled.div`
-  ${tw`w-full sm:w-1/2`};
-`;
+// export const Skill = styled.div`
+//   ${tw`w-full sm:w-1/2`};
+// `;
+
+export {};
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+// import React from 'react';
 
-import Layout from 'components/Layout';
-import SEO from 'components/SEO';
-import Posts from 'components/Posts';
+// import Layout from 'components/Layout';
+// import SEO from 'components/SEO';
+// import Posts from 'components/Posts';
 
-const BlogPage: React.FC = () => {
-  return (
-    <Layout>
-      <SEO title="Blog" />
-      <Posts />
-    </Layout>
-  );
-};
+// const BlogPage: React.FC = () => {
+//   return (
+//     <Layout>
+//       <SEO title="Blog" />
+//       <Posts />
+//     </Layout>
+//   );
+// };
 
-export default BlogPage;
+export default {};
